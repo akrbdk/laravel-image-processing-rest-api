fe935ad902238d22b4efe34b4988efa5e659a0be
alexander.karabudka <sanyakarabudka@gmail.com>
2023-05-21 14:45:18 +0300

    controllers, models and resources

---
+++ app/Http/Resources/V1/AlbumResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AlbumResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
